a7d232ac0fc4d46a4c92c102002540a55f2b7fa8
Michael Athanasoglou <mikedalpha@gmail.com>
2021-05-22 19:15:43 +0300

    init commit

---
+++ StopWatchDesign/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StopWatchDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 'start' to start the timer, 'stop' to stop the timer once started.");
+            string input;
+            do
+            {
+                input = Console.ReadLine().ToLower();
+                if (input == "start")
+                {
+                    Stopwatch.Start();
+                }
+                else if (input == "stop")
+                {
+                    Stopwatch.Stop();
+                }
+            }
+            while (input == "start" || input == "stop");
+        }
+    }
+}
---
+++ StopWatchDesign/Stopwatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StopWatchDesign
+{
+    class Stopwatch
+    {
+        private static DateTime startTime;
+        private static DateTime stopTime;
+        private static bool ticking;
+
+        public static void Start()
+        {
+            startTime = DateTime.Now;
+            if (ticking == true)
+            {
+                throw new InvalidOperationException("Stopwatch cannot be started twice in a row.");
+            }
+            Console.WriteLine("Stopwatch ticking... Press 'stop' to stop the timer.");
+            ticking = true;
+        }
+
+        public static void Stop()
+        {
+            stopTime = DateTime.Now;
+            if (ticking == false)
+            {
+                throw new InvalidOperationException("Stopwatch cannot be stopped twice in a row.");
+            }
+            Console.WriteLine($"Time elapsed: " + (stopTime - startTime) + ". Press 'start' again to restart the timer.");
+            ticking = false;
+        }
+    }
+}
